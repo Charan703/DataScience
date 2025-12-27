44b38c1694dd4866ba8fbb3b173c16ded996bfbd
Charan703 <charancherry70364@gmail.com>
2025-12-27 14:58:57 +0000

    model training stage

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.Data_Science import logger
 from src.Data_Science.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.Data_Science.pipeline.data_validation_piepline import DataValidationTrainingPipeline
 from src.Data_Science.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.Data_Science.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -28,6 +29,16 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.initiate_data_transformation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.initiate_model_training()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
--- src/Data_Science/pipeline/data_transformation_pipeline.py
+++ src/Data_Science/pipeline/data_transformation_pipeline.py
@@ -1,6 +1,7 @@
 from src.Data_Science.config.configuration import ConfigurationManager
 from src.Data_Science.components.data_transformation import DataTransformation
 from src.Data_Science import logger
+from pathlib import Path    
 
 STAGE_NAME = "Data Transformation stage"
 
@@ -9,10 +10,18 @@ class DataTransformationTrainingPipeline:
         pass
 
     def initiate_data_transformation(self):
-        config = ConfigurationManager()
-        data_transformation_config = config.get_data_transformation_config()
-        data_transformation = DataTransformation(config=data_transformation_config)
-        data_transformation.train_test_splitting()
+        try:
+            with open(Path("artifacts/data_validation/status.txt"), "r") as f:
+                status = f.read().split(" ")[-1]
+            if status == "True":
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_splitting()
+            else:
+                raise Exception("You data schema is not valid")
+        except Exception as e:
+            raise e
     
 if __name__ == '__main__':
     try:
